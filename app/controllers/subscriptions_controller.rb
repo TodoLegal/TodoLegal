be336aaf0520d3bbbf61034315e0ab8b64155d02
turupawn <ahmed.hn.43@gmail.com>
2019-11-24 20:44:05 -0600

    subscriptions and stuff

---
+++ app/controllers/subscriptions_controller.rb
@@ -0,0 +1,55 @@
+class SubscriptionsController < ApplicationController
+  def subscribe
+    email = params[:email]
+    @error_message = nil
+
+    if !email =~ Devise.email_regexp
+      @error_message = "Invalid mail, please try again."
+      return
+    end
+
+    if EmailSubscription.find_by_email(email)
+      return
+    end
+
+    email_subscription = EmailSubscription.new
+    email_subscription.email = email
+    email_subscription.security_key = SecureRandom.hex(25)
+    email_subscription.status = "pending"
+    email_subscription.save
+
+    SubscriptionsMailer.email_subscribed_email(email_subscription).deliver
+  end
+
+  def confirm_subscription
+    security_key = params[:security_key]
+
+    email_subscription = EmailSubscription.find_by_security_key(security_key)
+
+    @error_message = nil
+    if !email_subscription
+      @error_message = "Could not find your subscription."
+      return
+    end
+
+    email_subscription.status = "confirmed"
+    email_subscription.save
+
+    SubscriptionsMailer.email_confirmed_email(email_subscription).deliver
+  end
+
+  def unsubscribe
+    security_key = params[:security_key]
+
+    email_subscription = EmailSubscription.find_by_security_key(security_key)
+
+    @error_message = nil
+    if !email_subscription
+      @error_message = "Could not find your subscription."
+      return
+    end
+
+    email_subscription.status = "unsubscribed"
+    email_subscription.save
+  end
+end
---
+++ test/mailers/previews/subscriptions_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/subscriptions_mailer
+class SubscriptionsMailerPreview < ActionMailer::Preview
+
+end

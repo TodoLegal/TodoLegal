e8b3ec71c7711f1eb2f1562c6622157aa7fcbec8
turupawn <ahmed.hn.43@gmail.com>
2019-12-01 13:55:18 -0600

    e

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::ConfirmationsController < Devise::ConfirmationsController
+  include Devise::Controllers::Helpers
   # GET /resource/confirmation/new
   # def new
   #   super
--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::PasswordsController < Devise::PasswordsController
+  include Devise::Controllers::Helpers
   # GET /resource/password/new
   # def new
   #   super

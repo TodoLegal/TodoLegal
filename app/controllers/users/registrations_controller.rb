71a2d123324f9a332b4e22656b178a84d1ab2cbe
turupawn <ahmed.hn.43@gmail.com>
2019-12-01 13:11:00 -0600

    ww

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  skip_before_filter :verify_authenticity_token, :only => :create
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 

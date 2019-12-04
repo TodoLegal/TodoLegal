963570d42fbebcc5bf56a404521322ef4f6920e1
turupawn <ahmed.hn.43@gmail.com>
2019-12-03 23:31:12 -0600

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
-  include Devise::Controllers::Helpers
-  protect_from_forgery with: :null_session
+
+protected
+  def after_sign_up_path_for(resource)
+    items_path
+  end
+  def after_sign_in_path_for(resource)
+    if Setting.maintenance
+      return orders_path
+    end
+    items_path
+  end
 end

91f446ef27773d0fd88c47809a40ecb7041d691a
turupawn <ahmed.hn.43@gmail.com>
2019-12-03 23:35:08 -0600

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
-  
+
 protected
   def after_sign_up_path_for(resource)
-    items_path
+    root_path
   end
   def after_sign_in_path_for(resource)
-    if Setting.maintenance
-      return orders_path
-    end
-    items_path
+    root_path
   end
 end

c00b301058fe81b0d3ab5271f8b9d0b9dc5b4af9
turupawn <ahmed.hn.43@gmail.com>
2019-12-16 19:32:47 -0600

    see more & stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,12 @@ protected
   def after_sign_in_path_for(resource)
     root_path
   end
+
+  def findLaws query
+    @laws = Law.all.search_by_name(query).with_pg_search_highlight
+  end
+
+  def findArticles query
+    Article.all.search_by_body(query).group_by(&:law_id)
+  end
 end

c944111f5a8ea2482f18d01e3130b822e1a9034a
turupawn <ahmed.hn.43@gmail.com>
2020-02-23 17:42:41 -0600

    basic admin ui

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_11_004528) do
+ActiveRecord::Schema.define(version: 2020_02_23_221520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
@@ -45,6 +45,18 @@ ActiveRecord::Schema.define(version: 2020_02_11_004528) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "customer_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_customer_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customer_users_on_reset_password_token", unique: true
+  end
+
   create_table "email_subscriptions", force: :cascade do |t|
     t.string "email"
     t.string "security_key"
@@ -73,6 +85,12 @@ ActiveRecord::Schema.define(version: 2020_02_11_004528) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "modifications"
     t.string "creation_number"
+  end
+
+  create_table "permissions", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "sections", force: :cascade do |t|
@@ -111,6 +129,13 @@ ActiveRecord::Schema.define(version: 2020_02_11_004528) do
     t.string "name"
     t.string "number"
     t.integer "law_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "user_permissions", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "permission_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

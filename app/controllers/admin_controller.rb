309f5c1bc85c07ff28fe99991079921556dda122
turupawn <ahmed.hn.43@gmail.com>
2020-03-03 01:00:26 -0600

    manage permissions and see subscriptions permissions

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,8 @@
 class AdminController < ApplicationController
   skip_before_action :verify_authenticity_token
-  before_action :authenticate_admin!
-  
+  before_action :user_can_manage_permissions!, only: [:users, :grant_permission, :revoke_permission]
+  before_action :user_can_see_subscriptions!, only: [:subscriptions]
+
   def users
     @email = params[:email]
     if @email

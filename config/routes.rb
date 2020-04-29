ba50ecafd5056ab64bf2949ad2458ed4d77d1b22
turupawn <ahmed.hn.43@gmail.com>
2020-04-29 01:07:40 -0600

    drive search

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   get '/search_law', to: 'home#search_law'
   get '/terms', to: 'home#terms_and_conditions'
   get '/pricing', to: 'home#pricing'
+  get '/drive_search', to: 'home#drive_search'
 
   post "subscribe" => "subscriptions#subscribe", as: "subscribe"
   get "unsubscribe" => "subscriptions#unsubscribe", as: "unsubscribe"

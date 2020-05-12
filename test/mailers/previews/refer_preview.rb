1694789542584bd80c61ddce1564f25d40d697d7
turupawn <ahmed.hn.43@gmail.com>
2020-05-12 10:10:41 -0600

    referal + ui improvements

---
+++ test/mailers/previews/refer_preview.rb
@@ -0,0 +1,6 @@
+# Preview all emails at http://localhost:3000/rails/mailers/subscriptions_mailer
+class SubscriptionsMailerPreview < ActionMailer::Preview
+  def refer
+    SubscriptionsMailer.refer(User.first)
+  end
+end

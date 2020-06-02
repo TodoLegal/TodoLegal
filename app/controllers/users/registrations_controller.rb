22b6a2a0ad90f90c448a2002040a7fffb30053f4
Carlos Villanueva <carlosvilla00896@gmail.com>
2020-06-02 03:47:52 -0600

    Revert "Merge pull request #27 from TodoLegal/master"
    This reverts commit 55223b5837bf0e79195e19b1f2f2e2a1ad2a70a4, reversing
    changes made to e34e1b4abfce32823bd3b7a8d09814edc2087e96.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -74,12 +74,4 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
-
-  def update_resource(resource, params)
-    if params["password"]&.present? or params["email"]&.present?
-      return super
-    else
-      resource.update_without_password(params.except("current_password"))
-    end
-  end
 end

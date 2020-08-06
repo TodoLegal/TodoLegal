d93b6a7c5cdaa703706ebf425db5e0a579865c5b
turupawn <ahmed.hn.43@gmail.com>
2020-08-06 00:03:42 -0600

    fix

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -18,7 +18,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # POST /resource
   def create
     validateOccupationParam (params)
-    $discord_bot.send_message($discord_bot_channel, "Se ha registrado un nuevo usuario :tada:")
     super
   end
 
@@ -61,6 +60,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
+    $discord_bot.send_message($discord_bot_channel, "Se ha registrado un nuevo usuario :tada:")
     session[:user_just_signed_up] = true
     root_path
     # invite_friends_path(is_onboarding:true)

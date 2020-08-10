a1cf955ee49dbad7c6624965cd97f0ce414fed8f
turupawn <ahmed.hn.43@gmail.com>
2020-08-10 10:52:34 -0600

    welcome pro user ToDo

--- app/mailers/subscriptions_mailer.rb
+++ app/mailers/subscriptions_mailer.rb
@@ -14,4 +14,8 @@ class SubscriptionsMailer < ApplicationMailer
     end
     mail(from: 'TodoLegal <suscripciones@todolegal.app>', to: email, subject: 'Te han invitado a TodoLegal')
   end
+
+  def welcome_pro_user user
+    mail(from: 'TodoLegal <suscripciones@todolegal.app>', to: user.email, subject: 'ToDo subject')
+  end
 end

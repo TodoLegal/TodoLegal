27d8c63693453a3551e206d01f0709a15c65e483
turupawn <ahmed.hn.43@gmail.com>
2020-10-07 18:45:24 +0900

    production env launch on test fix

--- app/controllers/api/v1/examples_controller.rb
+++ app/controllers/api/v1/examples_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::ExamplesController < Api::V1::ApplicationController
+class Api::V1::ExamplesController < ApplicationController
   protect_from_forgery with: :null_session
 
   def action_test

85053e83fe424e90d42e80b331d08e8e7f77cafe
turupawn <ahmed.hn.43@gmail.com>
2020-12-04 18:21:54 +0900

    document tags

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,9 @@
 class Document < ApplicationRecord
   include PgSearch
 
+  has_many :document_tags
+  has_many :tags, through: :document_tags, :dependent => :destroy
+
   has_one_attached :original_file
 
   pg_search_scope :search_by_all,

b3acc2fd203d1eb449ebd9fb283aedefa75c1db0
turupawn <ahmed.hn.43@gmail.com>
2021-01-05 17:45:43 +0900

    better parametterize for publication number

--- app/models/document.rb
+++ app/models/document.rb
@@ -16,6 +16,6 @@ class Document < ApplicationRecord
                   }
 
   def generate_friendly_url
-    [id, name.parameterize, publication_number.parameterize].join('-')
+    [id, name.parameterize, publication_number.parameterize.tr('-','')].join('-')
   end
 end

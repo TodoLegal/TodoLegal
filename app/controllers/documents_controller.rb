33160f3761733ec3a7f17830fee62eb0deccb89b
turupawn <ahmed.hn.43@gmail.com>
2021-01-24 10:51:31 +0900

    gcs bucket env var

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -36,7 +36,7 @@ class DocumentsController < ApplicationController
         # download file
         require "google/cloud/storage"
         storage = Google::Cloud::Storage.new(project_id:"docs-tl", credentials: Rails.root.join("gcs.keyfile"))
-        bucket = storage.bucket "docsbytodolegal"
+        bucket = storage.bucket GCS_BUCKET
         file = bucket.file @document.original_file.key
         if params["document"]["process_gazette"] == true
           file.download "tmp/gazette.pdf"

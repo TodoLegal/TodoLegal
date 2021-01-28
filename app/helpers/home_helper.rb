45826ad3e671ac0dad1bd850338954fea191c99a
turupawn <ahmed.hn.43@gmail.com>
2021-01-29 08:01:33 +0900

    redirect to valid

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,5 +1,6 @@
 module HomeHelper
   def get_google_drive_search_back_link_path
+    # TODO: Remove this
     if @folder && @folder!=""
       return google_drive_search_path + "?get_parent_files=true&folder=" + @folder
     elsif @query && @query!=""

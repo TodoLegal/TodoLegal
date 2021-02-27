e94e6d6bc3bdf35c889100971c235c32b95c9762
turupawn <ahmed.hn.43@gmail.com>
2021-02-27 15:20:38 -0600

    added master key deploy key

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,20 @@ lock "~> 3.15.0"
 
 set :application, "TodoLegal"
 set :repo_url, "https://github.com/TodoLegal/TodoLegal.git"
-set :linked_files, %w{config/master.key}
+
+append :linked_files, "config/master.key"
+
+namespace :deploy do
+  namespace :check do
+    before :linked_files, :set_master_key do
+      on roles(:app), in: :sequence, wait: 10 do
+        unless test("[ -f #{shared_path}/config/master.key ]")
+          upload! 'config/master.key', "#{shared_path}/config/master.key"
+        end
+      end
+    end
+  end
+end
 
 # Deploy to the user's home directory
 set :deploy_to, "/home/deploy/#{fetch :application}"

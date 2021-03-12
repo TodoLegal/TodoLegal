3cfa17a7efdb42efa19538f5aa004b00d7fb7c96
carlosvilla00896 <carlosvilla00896@gmail.com>
2021-03-12 02:57:05 -0600

    added short description and full text field to Documents table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_221938) do
+ActiveRecord::Schema.define(version: 2021_03_12_084336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
@@ -97,6 +97,8 @@ ActiveRecord::Schema.define(version: 2021_03_04_221938) do
     t.date "publication_date"
     t.string "publication_number"
     t.text "description"
+    t.text "short_description"
+    t.text "full_text"
   end
 
   create_table "email_subscriptions", force: :cascade do |t|

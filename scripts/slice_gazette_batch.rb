77c4d8ee129d471aa16aec36768e7271829a5000
turupawn <ahmed.hn.43@gmail.com>
2021-03-18 18:24:03 -0600

    add gazette scripts

--- scripts/slice_gazette_batch.rb
+++ scripts/slice_gazette_batch.rb
@@ -1,11 +1,11 @@
 #how to run:
 #ARGV = ['/home/turupawn/Projects/Gacetas/']
-#load 'load_gazette_batch.rb'
+#load 'scripts/slice_gazette_batch.rb'
 dir_path = ARGV[0]
 Dir.each_child(dir_path) {
     |filename|
     puts "Loading.."
     ARGV[0] = "\"" + dir_path + filename + "\""
-    load 'scripts/load_gazette.rb'
+    load 'scripts/slice_gazette.rb'
     puts "Loaded!"
 }

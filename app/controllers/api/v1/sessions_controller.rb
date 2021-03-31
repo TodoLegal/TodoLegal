485bf98d4c0cab853bda38d47a818987e8cbfa61
turupawn <ahmed.hn.43@gmail.com>
2021-03-31 11:32:38 -0600

    api: added me

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::SessionsController < Devise::SessionsController
   #skip_before_action :verify_signed_out_user, only[:destroy]
   protect_from_forgery with: :null_session
+  include ApplicationHelper
+  before_action :doorkeeper_authorize!, only: [:me]
 
   def create
     user = warden.authenticate!({user: params[:user]})
@@ -33,5 +35,22 @@ class Api::V1::SessionsController < Devise::SessionsController
       end
     end
   end
-  
+
+  def me
+    user = User.find_by_id(doorkeeper_token.resource_owner_id)
+    customer = Stripe::Customer.retrieve(user.stripe_customer_id)
+    current_user_type = "not logged"
+    if current_user_plan_is_active customer
+      current_user_type = "basic"
+    else
+      current_user_type = "pro"
+    end
+    render json: {"document": json_document,
+      "tags": get_document_tags,
+      "related_documents": get_related_documents,
+      "downloads": user_document_visit_tracker.visits,
+      "can_access": can_access_document,
+      "user_type": current_user_type
+    }
+  end
 end

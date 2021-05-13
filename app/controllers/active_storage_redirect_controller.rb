ff3d915817e05293e9b6aa1cfb5fddcf35ed72fc
Hector Aguilar <haguilar91@gmail.com>
2021-05-13 23:53:23 +0000

    Cambios para correr ElasticSearch en server externo

--- app/controllers/active_storage_redirect_controller.rb
+++ app/controllers/active_storage_redirect_controller.rb
@@ -6,16 +6,12 @@ class ActiveStorageRedirectController < ActiveStorage::Blobs::RedirectController
   skip_before_action :doorkeeper_authorize!, unless: :has_access_token?
 
   def show
-    user_id_str = ""
     if params[:access_token]
       user = User.find_by_id(doorkeeper_token.resource_owner_id)
-      user_id_str = user.id.to_s
     end
-    user_document_visit_tracker = get_user_document_visit_tracker(user_id_str)
+    user_document_visit_tracker = get_user_document_visit_tracker
     can_access_document = can_access_documents(user_document_visit_tracker, current_user_type(user))
-    if current_user_type(user) != "pro"
-      user_document_visit_tracker.visits += 1
-    end
+    user_document_visit_tracker.visits += 1
     if can_access_document
       user_document_visit_tracker.save
       super

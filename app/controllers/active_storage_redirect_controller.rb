fb848663c3adbd63ce4b50353453d9e98942ba6c
turupawn <ahmed.hn.43@gmail.com>
2021-05-18 11:15:43 -0600

    renamed visits to downloads

--- app/controllers/active_storage_redirect_controller.rb
+++ app/controllers/active_storage_redirect_controller.rb
@@ -11,13 +11,13 @@ class ActiveStorageRedirectController < ActiveStorage::Blobs::RedirectController
       user = User.find_by_id(doorkeeper_token.resource_owner_id)
       user_id_str = user.id.to_s
     end
-    user_document_visit_tracker = get_user_document_visit_tracker(user_id_str)
-    can_access_document = can_access_documents(user_document_visit_tracker, current_user_type(user))
+    user_document_download_tracker = get_user_document_download_tracker(user_id_str)
+    can_access_document = can_access_documents(user_document_download_tracker, current_user_type(user))
     if current_user_type(user) != "pro"
-      user_document_visit_tracker.visits += 1
+      user_document_download_tracker.downloads += 1
     end
     if can_access_document
-      user_document_visit_tracker.save
+      user_document_download_tracker.save
       super
     else
       redirect_to "http://valid.todolegal.app?error='invalid permissions'"
--- app/models/user_document_visit_tracker.rb
+++
@@ -1,2 +0,0 @@
-class UserDocumentVisitTracker < ApplicationRecord
-end

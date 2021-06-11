bedd73bc71c6e3515970a50b816601b0f8fcbcef
turupawn <ahmed.hn.43@gmail.com>
2021-06-10 18:15:10 -0600

    added issuer law tag model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_10_235654) do
+ActiveRecord::Schema.define(version: 2021_06_11_001419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
@@ -115,6 +115,14 @@ ActiveRecord::Schema.define(version: 2021_06_10_235654) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["document_id", "tag_id"], name: "index_issuer_document_tags_on_document_id_and_tag_id", unique: true
+  end
+
+  create_table "issuer_law_tags", force: :cascade do |t|
+    t.integer "law_id"
+    t.integer "tag_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["law_id", "tag_id"], name: "index_issuer_law_tags_on_law_id_and_tag_id", unique: true
   end
 
   create_table "law_accesses", force: :cascade do |t|

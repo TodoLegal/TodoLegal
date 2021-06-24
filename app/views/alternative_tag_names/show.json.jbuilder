35d44a3005a5f0f244e4a32682421fcdffa9442a
turupawn <ahmed.hn.43@gmail.com>
2021-06-24 13:47:24 -0600

    added alternative tag name

---
+++ app/views/alternative_tag_names/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.partial! "alternative_tag_names/alternative_tag_name", alternative_tag_name: @alternative_tag_name
---
+++ test/models/alternative_tag_name_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class AlternativeTagNameTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end

f6f127d554546352c79b3956fcc6a75afa2217a1
Hector Aguilar <haguilar91@gmail.com>
2021-08-18 20:50:50 -0600

    Update deploy.rb
    Removed rbenv-vars from Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,6 @@ set :passenger_restart_command, '/usr/bin/passenger-config restart-app'
 append :linked_files, "config/master.key"
 append :linked_files, "config/credentials.yml.enc"
 append :linked_files, "gcs.keyfile"
-append :linked_files, ".rbenv-vars"
 
 namespace :deploy do
   namespace :check do

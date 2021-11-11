4eb1d9d83fc5feb75d21588824316ed0698852c4
Hector Aguilar <hector@todolegal.app>
2021-11-11 16:07:52 +0000

    Fixing ENV server port

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -10,8 +10,7 @@ set :branch, ENV["CAPISTRANO_BRANCH"]
 server ENV["SERVER_IP"], user: 'deploy', roles: %w{app db web}
 set :ssh_options, {
     forward_agent: true
-  , port: %w{ENV["SERVER_PORT"]}
-
+  , port: ENV["SERVER_PORT"]
 }
 
 # role-based syntax

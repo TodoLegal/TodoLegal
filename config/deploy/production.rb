53829e0d775253093af2c187e20a928ede240f6e
Hector Aguilar <hector@todolegal.app>
2021-11-11 18:36:18 +0000

    Added Gem to manage CAP ports and added the vars there

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,12 +7,12 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 set :branch, ENV["CAPISTRANO_BRANCH"]
-server ENV["SERVER_IP"], user: 'deploy', port:ENV["SERVER_PORT"] ,roles: %w{app db web}
-#set :ssh_options, {
-#    forward_agent: true,
-#    port: ENV["SERVER_PORT"],
-#    user: 'deploy'
-#}
+server ENV["SERVER_IP"], user: 'deploy' ,roles: %w{app db web}
+set :ssh_options, {
+    forward_agent: true,
+    port: ENV["SERVER_PORT"],
+    user: 'deploy'
+}
 
 # role-based syntax
 # ==================

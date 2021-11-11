0cfb88f37b2dcd1520731b53544dc1214eb1af08
Hector Aguilar <hector@todolegal.app>
2021-11-11 15:53:53 +0000

    Correcting server port call (was calling server ip on port)

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,7 +8,8 @@
 # server "db.example.com", user: "deploy", roles: %w{db}
 set :branch, ENV["CAPISTRANO_BRANCH"]
 server ENV["SERVER_IP"], user: 'deploy', roles: %w{app db web}
-set :ssh_options, {forward_agent: true, port:ENV["SERVER_IP"], user: 'deploy'}
+set :ssh_options, {forward_agent: true, port:ENV["SERVER_PORT"], user: 'deploy'}
+
 
 # role-based syntax
 # ==================

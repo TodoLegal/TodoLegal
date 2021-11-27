445dc4d05028097222215515607af3910d5c4f0f
Ahmed Castro <ahmed.hn.43@gmail.com>
2021-11-26 20:38:40 -0600

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,9 +25,9 @@ namespace :deploy do
       end
     end
   end
-  task :link_relic, :roles => :app do
-    run "ln -s /home/deploy/newrelic.yml /home/deploy/TodoLegal/current/config"
-  end
+  #task :link_relic, :roles => :app do
+  #  run "ln -s /home/deploy/newrelic.yml /home/deploy/TodoLegal/current/config"
+  #end
 end
 
 # after 'deploy:foo', 'deploy:link_relic'

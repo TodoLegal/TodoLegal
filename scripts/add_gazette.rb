402411d453cac870586e37b4dea5691df6e07ec7
Ahmed Castro <ahmed.hn.43@gmail.com>
2021-11-30 17:55:30 -0600

    updated script for document types

--- scripts/add_gazette.rb
+++ scripts/add_gazette.rb
@@ -6,7 +6,7 @@ file_path = ARGV[0]
 puts "Loading.."
 puts file_path
 document_controller = DocumentsController.new
-@document = Document.create
+@document = Document.create(document_type_id: DocumentType.find_by_name("Gaceta").id)
 document_controller.slice_gazette @document, '"' + file_path + '"'
 puts "A"
 @document.original_file.attach(

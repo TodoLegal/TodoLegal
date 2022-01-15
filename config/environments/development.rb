6a242c786b470c4b1ca900b27d74b4e7e820ca47
Hector Aguilar <haguilar91@gmail.com>
2022-01-15 12:04:25 -0600

    Applying Mailgun Config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,6 +32,14 @@ Rails.application.configure do
   config.active_storage.service = :google
   config.hosts << "161.35.177.91"
   # Don't care if the mailer can't send.
+  config.action_mailer.default_url_options = { host: ENV['MAILGUN_URL'] }
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.mailgun_settings = {
+    api_key: ENV['MAILGUN_KEY'],
+    domain: ENV['MAILGUN_URL']
+  # api_host: 'api.eu.mailgun.net'  # Uncomment this line for EU region domains
+  }
+
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
@@ -60,5 +68,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { host: "test.todolegal.app" }
+  
+
 end

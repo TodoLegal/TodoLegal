414de485cb30b677403379eca307c5e9fa8a23d9
Hector Aguilar <haguilar91@gmail.com>
2022-01-15 13:07:46 -0600

    Update subscriptions_mailer.rb

--- app/mailers/subscriptions_mailer.rb
+++ app/mailers/subscriptions_mailer.rb
@@ -1,5 +1,5 @@
 class SubscriptionsMailer < ApplicationMailer
-  default from: 'TodoLegal <suscripciones@todolegal.app>'
+  default from: 'TodoLegal <suscripciones@mg.todolegal.app>'
 
   def is_a_valid_email?(email)
     #email_regex = %r{/\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/}xi # Case insensitive
@@ -12,13 +12,13 @@ class SubscriptionsMailer < ApplicationMailer
       @referrer_name += " "
       @referrer_name += referrer.last_name
     end
-    mail(from: 'TodoLegal <suscripciones@todolegal.app>', to: email, subject: 'Te han invitado a TodoLegal')
+    mail(from: 'TodoLegal <suscripciones@mg.todolegal.app>', to: email, subject: 'Te han invitado a TodoLegal')
   end
 
   def welcome_pro_user user
     @user = user
     # TODO: make this auto calculated again
     @rounded_all_document_count = 1130
-    mail(from: 'TodoLegal <suscripciones@todolegal.app>', to: user.email, subject: 'Cuenta Pro')
+    mail(from: 'TodoLegal <suscripciones@mg.todolegal.app>', to: user.email, subject: 'Cuenta Pro')
   end
 end

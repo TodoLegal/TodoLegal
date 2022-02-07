fc1f3199b938d056250b783fa46ca8128e803ec6
carlosvilla00896 <carlosvilla00896@gmail.com>
2022-02-07 16:39:22 -0600

    fixs MixPanel

--- app/controllers/api/v1/documents_controller.rb
+++ app/controllers/api/v1/documents_controller.rb
@@ -101,19 +101,6 @@ class Api::V1::DocumentsController < ApplicationController
         offset: params["offset"].to_i,
         order: {publication_date: :desc})
     end
-
-    p "Search response: ", documents
-    
-    if !params["query"].blank?
-      $tracker.track(0, 'Valid Search', {
-        'query' => query,
-        'location' => "API",
-        'limit' => limit,
-        'offset' => params["offset"],
-        'tags' => params["tags"],
-        'results' => total_count
-      })
-    end
     
     total_count = documents.total_count
     documents = documents.to_json
@@ -136,6 +123,17 @@ class Api::V1::DocumentsController < ApplicationController
       document_json_post_process document["id"].to_i, document
     end
 
+    if !params["query"].blank?
+      $tracker.track(0, 'Valid Search', {
+        'query' => query,
+        'location' => "API",
+        'limit' => limit,
+        'offset' => params["offset"],
+        'tags' => params["tags"],
+        'results' => total_count
+      })
+    end
+    
     render json: { "documents": documents, "count": total_count }
   end
 

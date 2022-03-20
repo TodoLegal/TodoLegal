0b55a725818164436dfdf3e6931c3a7ae7f0fe7f
Deploy User <deploy@ip-172-31-23-90.us-east-2.compute.internal>
2022-03-20 05:41:00 +0000

    Only 1 session per user implementation, cache, and added column for unique session id

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :validatable, :trackable
+         :recoverable, :rememberable, :validatable, :trackable,
+         :session_limitable
   
   acts_as_token_authenticatable
 

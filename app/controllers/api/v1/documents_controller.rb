a3631c2816aec2603131ffae85e1b6442def2523
Deploy User <deploy@ip-172-31-23-90.us-east-2.compute.internal>
2022-03-25 05:58:47 +0000

    test 5

--- app/controllers/api/v1/documents_controller.rb
+++ app/controllers/api/v1/documents_controller.rb
@@ -206,6 +206,21 @@ protected
   end
 
 def already_logged_in
+  Warden::Manager.after_set_user except: :fetch do |record, warden, options|
+    if record.devise_modules.include?(:session_limitable) &&
+       warden.authenticated?(options[:scope]) &&
+       !record.skip_session_limitable?
+  
+       if !options[:skip_session_limitable]
+        unique_session_id = Devise.friendly_token
+        warden.session(options[:scope])['unique_session_id'] = unique_session_id
+        record.update_unique_session_id!(unique_session_id)
+       else
+        warden.session(options[:scope])['devise.skip_session_limitable'] = true
+       end
+    end
+  end
+  
   Warden::Manager.after_set_user only: :fetch do |record, warden, options|
     scope = options[:scope]
     if record.devise_modules.include?(:session_limitable) &&

d0aa591603da16b271e93c065ff305fbd386a851
carlosvilla00896 <carlosvilla00896@gmail.com>
2022-04-19 21:48:40 -0600

    code cleanup

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,7 +3,6 @@ class Api::V1::SessionsController < Devise::SessionsController
   protect_from_forgery with: :null_session
   include ApplicationHelper
   before_action :doorkeeper_authorize!, only: [:me]
-  before_action :already_logged_in
 
   def create
     user = warden.authenticate!({user: params[:user]})
@@ -43,30 +42,6 @@ class Api::V1::SessionsController < Devise::SessionsController
       "user_type": current_user_type_api(user),
       "confirmed_user": user ? user.confirmed_at? : false
     }
-  end
-
-  def already_logged_in
-    Warden::Manager.after_set_user only: :fetch do |record, warden, options|
-      scope = options[:scope]
-      if record.devise_modules.include?(:session_limitable) &&
-        warden.authenticated?(scope) &&
-        options[:store] != false
-       if record.unique_session_id != warden.session(scope)['unique_session_id'] &&
-          !record.skip_session_limitable? && 
-          !warden.session(scope)['devise.skip_session_limitable']
-         Rails.logger.warn do
-           '[devise-security][session_limitable] session id mismatch: '\
-           "expected=#{record.unique_session_id.inspect} "\
-           "actual=#{warden.session(scope)['unique_session_id'].inspect}"
-         end
-        #  redirect_to '/users/edit'
-         warden.raw_session.clear
-         warden.logout(scope)
-         throw :warden, scope: scope, message: :session_limited
-         
-       end
-      end
-    end
   end
 
 end

7936a936f3f0dc46521df4cc3714741eee56282d
carlosvilla00896 <carlosvilla00896@gmail.com>
2022-06-19 03:32:16 -0600

    adds doorkeeper authorization

--- app/controllers/api/v1/users_preferences_controller.rb
+++ app/controllers/api/v1/users_preferences_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::UsersPreferencesController < ApplicationController
     protect_from_forgery with: :null_session
     include ApplicationHelper
-    # before_action :doorkeeper_authorize!, only: [:get_user_preferences, :update_user_preferences]
+    before_action :doorkeeper_authorize!, only: [:get_user_preferences, :update_user_preferences]
 
     def get_user_preferences
         user_preferences = []

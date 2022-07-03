943b8b6d2199aca56642c64b160ea8bee11c3379
carlosvilla00896 <carlosvilla00896@gmail.com>
2022-07-03 04:15:46 -0600

    adds dynamic email generator

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -8,11 +8,18 @@ class NotificationsMailer < ApplicationMailer
   end
 
   def user_preferences_mail(user, notif_arr) 
-      @user = user
-      @documents_to_send = notif_arr
+      @user = User.find_by(email: "carlosvilla00896@gmail.com")
+      @documents_to_send = []
+      @tema_tag_id = TagType.find_by(name: "tema").id
+      @materia_tag_id = TagType.find_by(name: "materia").id
+      docs = DocumentTag.joins(:document).where(document_tags: {tag_id: @tema_tag_id }).or( DocumentTag.joins(:document).where(document_tags: {tag_id: @materia_tag_id }) ).select(:tag_id, :document_id, :name, :issue_id, :publication_number, :publication_date, :description)
+      # docs = docs.group(:tag_id)
+      docs = docs.order(tag_id: :asc)
+      docs.each do |doc|
+        @documents_to_send.push(doc)
+      end
       #Controllers/ActiveStorageMailer have the jobs.
-      #mail(from: 'TodoLegal <suscripciones@todolegal.app>', to: user.email, subject: 'Sus leyes importantes.')
-      mail(from: 'TodoLegal <suscripciones@todolegal.app>', to: "hector@todolegal.app", subject: 'Sus leyes importantes.')
+      mail(from: 'TodoLegal <suscripciones@todolegal.app>', to: @user.email, subject: 'Sus leyes importantes.')
   end
 
 end

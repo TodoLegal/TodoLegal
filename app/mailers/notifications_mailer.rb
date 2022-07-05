f72df0bdd8202c0eac5468f6f774a63f2cae1d3e
carlosvilla00896 <carlosvilla00896@gmail.com>
2022-07-05 03:18:59 -0600

    adds docs clasification algorithm to notifications mailer

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -9,17 +9,32 @@ class NotificationsMailer < ApplicationMailer
 
   def user_preferences_mail(user, notif_arr) 
       @user = User.find_by(email: "carlosvilla00896@gmail.com")
-      @documents_to_send = []
+
       @tema_tag_id = TagType.find_by(name: "tema").id
       @materia_tag_id = TagType.find_by(name: "materia").id
-      docs = DocumentTag.joins(:document).where(document_tags: {tag_id: @tema_tag_id }).or( DocumentTag.joins(:document).where(document_tags: {tag_id: @materia_tag_id }) ).select(:tag_id, :document_id, :name, :issue_id, :publication_number, :publication_date, :description)
-      # docs = docs.group(:tag_id)
+      docs = Document.joins(:document_tags).select(:tag_id, :document_id, :name, :issue_id, :publication_number, :publication_date, :description)
+
       docs = docs.order(tag_id: :asc)
+      @documents_to_send = []
+      current_tag_name = ""
+      temp_docs = []
+
       docs.each do |doc|
-        @documents_to_send.push(doc)
+        tag_name = Tag.find_by(id: doc.tag_id).name
+        if tag_name != current_tag_name
+          if temp_docs.length > 0
+            @documents_to_send.push({
+              "tag_name": current_tag_name,
+              "documents": temp_docs
+            })
+            temp_docs = []
+          end
+          current_tag_name = tag_name
+        end
+        temp_docs.push(doc)
       end
       #Controllers/ActiveStorageMailer have the jobs.
-      mail(from: 'TodoLegal <suscripciones@todolegal.app>', to: @user.email, subject: 'Sus leyes importantes.')
+      mail(from: 'TodoLegal <suscripciones@todolegal.app>', to: @user.email, subject: 'Notificaciones personalizadas.')
   end
 
 end

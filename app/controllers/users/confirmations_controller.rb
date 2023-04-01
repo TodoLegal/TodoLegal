618e162cdc5fd07a8c025beb3e56eeba38662308
carlosvilla00896 <carlosvilla00896@gmail.com>
2023-03-31 18:47:23 -0600

    customize after_confirmation_path_for

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -15,13 +15,9 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
     end
   end
 
-  def after_confirmation_path_for
-   if process_doorkeeper_redirect_to
-      return
-   else
-      root_path
-   end
-  end
+  #def after_confirmation_path_for(resource)
+  #  root_path
+  #end
 
   def show
     self.resource = resource_class.confirm_by_token(params[:confirmation_token])
@@ -29,7 +25,7 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
 
     if resource.errors.empty?
       set_flash_message!(:notice, :confirmed)
-      redirect_to after_confirmation_path_for
+      respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
     else
       #respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
       set_flash_message!(:notice, :already_confirmed)

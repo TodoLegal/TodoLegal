90c1ad428a36e2922d54bddc4139779fde382a60
carlosvilla00896 <carlosvilla00896@gmail.com>
2023-04-18 03:36:59 -0600

    some changes and fixes

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,10 +1,53 @@
 class NotificationsMailer < ApplicationMailer
   default from: 'TodoLegal <suscripciones@todolegal.app>'
   helper ApplicationHelper
+  include ApplicationHelper
 
   def is_a_valid_email?(email)
     #email_regex = %r{/\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/}xi # Case insensitive
     #(email =~ email_regex)
+  end
+
+  def tags_test_data
+
+    @docs_array = []
+    @tags_array = [1, 3, 5, 17, 51]
+
+    @tema_tag_id = TagType.find_by(name: "tema").id
+    @materia_tag_id = TagType.find_by(name: "materia").id
+
+    docs = []
+    @tags_array.each do |tag|
+      temp = Document.joins(:document_tags).where('publication_date > ?',(Date.today - 3000.day).to_datetime).where('document_tags.tag_id'=> tag).select(:tag_id, :id, :name, :issue_id, :publication_number, :publication_date, :description, :url)
+        
+      if temp.length > 0
+        temp.each do | doc |
+          docs.push( doc )
+        end
+      end
+
+    end
+
+    docs.each do |doc|
+      @docs_array.push(doc)
+    end
+
+    docs = @docs_array.sort_by{|item| item.tag_id }
+
+    return docs
+  end
+
+  def remaining_trial user
+    user_trial = user.user_trial
+
+    if !user_trial && current_user_type_api(user) != "pro"
+      user_trial = UserTrial.create(user_id: user.id, trial_start: DateTime.now, trial_end: DateTime.now + 1.day, active: true)
+      # NotificationsMailer.basic_with_active_notifications(user).deliver
+      # SubscriptionsMailer.free_trial_end(user).deliver_later(wait_until: user_trial.trial_end - 12.hours)
+      # NotificationsMailer.cancel_notifications(user).deliver_later(wait_until: user_trial.trial_end)
+    end
+
+    return remaining_free_trial_time(user)
   end
 
   def user_preferences_mail(user, notif_arr) 
@@ -12,8 +55,12 @@ class NotificationsMailer < ApplicationMailer
       @user_preferences = UsersPreference.find_by(user_id: @user.id)
       @user_notifications_history = UserNotificationsHistory.find_by(user_id: @user.id)
       @documents_to_send = []
+      @remaining_free_trial_time = remaining_trial(@user)
+      @user_type = current_user_type_api(@user)
+      
+      #change this line to get data from tags_test_data method when testing
+      docs = notif_arr.sort_by{|item| item.tag_id}
 
-      docs = notif_arr.sort_by{|item| item.tag_id }
       current_tag_name = ""
       temp_docs = []
 

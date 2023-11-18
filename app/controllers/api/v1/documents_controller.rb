c1dc067c50086428ad11b908304de7ac6786124a
Hector Aguilar <haguilar91@gmail.com>
2023-11-18 04:50:17 +0000

    update searchkick logic

--- app/controllers/api/v1/documents_controller.rb
+++ app/controllers/api/v1/documents_controller.rb
@@ -110,7 +110,7 @@ class Api::V1::DocumentsController < ApplicationController
         fields: ["name^10", "issue_id^5", "short_description^2", "description"],
         where: searchkick_where,
         misspellings: {edit_distance: 2, below: 5},
-        limit: limit,
+        limit: 2,
         offset: params["offset"].to_i)
     else
       documents = Document.search(
@@ -274,10 +274,11 @@ protected
   end
 
   def attach_file_to_documents documents, can_access
-    docs = documents
+    document_hash = documents
+    query_array   = document_hash["query"]
 
-    docs.each do | document |
-      ar_document = Document.find_by_id(document["id"])
+    query_array.each do | document |
+      ar_document = Document.find_by_id(document['id'])
       if can_access && ar_document.original_file.attached?
         document["file"] = url_for(ar_document.original_file)
         document["can_access"] = true
@@ -287,7 +288,7 @@ protected
       end
     end
 
-    return docs
+    return query_array
   end
 
 end

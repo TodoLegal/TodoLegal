53effc0d984c4514d94fc91f977aa49b6709ad8c
Hector Aguilar Herdocia <haguilar91@gmail.com>
2023-12-04 19:04:27 -0600

    Update cors.rb
    Restore Cors file.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,15 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+  end
+end
+
+Rails.application.config.hosts << "test.todolegal.app"
+Rails.application.config.hosts << "todolegal.app"
+Rails.application.config.hosts << "devchuco.todolegal.app"
+Rails.application.config.hosts << "167.71.188.59"

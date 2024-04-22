00f15dea9093e91809cfabcd8e1d3eab4d50dbf7
carlosvilla00896 <carlosvilla00896@gmail.com>
2024-04-22 15:01:45 -0600

    some fixes

--- app/jobs/document_processing_job.rb
+++ app/jobs/document_processing_job.rb
@@ -101,38 +101,43 @@ include DocumentsHelper
     end
     puts "Created related documents"
     document_link = "https://test.todolegal.app/admin/gazettes/#{document.publication_number}"
+    process_status = "success"
     # if $discord_bot
     #   publication_number = @document.publication_number
     #   discord_message = "Nueva gaceta seccionada en Valid! [#{publication_number}](https://todolegal.app/admin/gazettes/#{publication_number}) :scroll:"
     #   $discord_bot.send_message($discord_bot_document_upload, discord_message)
     # end
-    DocumentProcessingMailer.document_processing_complete(user, document_link).deliver
+    DocumentProcessingMailer.document_processing_complete(user, document_link, process_status).deliver
   end
 
   private
 
   def run_slice_gazette_script document, document_pdf_path
     puts ">run_slice_gazette_script called"
-    python_return_value = `python3 /home/carlosvilla/Github/TodoLegal-Repos/GazetteSlicer/slice_gazette.py #{ document_pdf_path } '#{ Rails.root.join("public", "gazettes") }' '#{document.id}'`
+    python_return_value = `python3 ~/GazetteSlicer/slice_gazette.py #{ document_pdf_path } '#{ Rails.root.join("public", "gazettes") }' '#{document.id}'`
     begin
       result = JSON.parse(python_return_value)
       return result
     rescue
       document.description = "Error: on slice gazette"
       document.save
+      process_status = "error"
+      DocumentProcessingMailer.document_processing_complete(user, document_link, process_status).deliver
       return {}
     end
   end
 
   def process_gazette document, document_pdf_path
     puts ">process_gazette called"
-    python_return_value = `python3 /home/carlosvilla/Github/TodoLegal-Repos/GazetteSlicer/process_gazette.py #{ document_pdf_path }`
+    python_return_value = `python3 ~/GazetteSlicer/slice_gazette.py #{ document_pdf_path }`
     json_data = {}
     begin
       json_data = JSON.parse(python_return_value)
     rescue
       document.description = "Error: on process gazette"
       document.save
+      process_status = "error"
+      DocumentProcessingMailer.document_processing_complete(user, document_link, process_status).deliver
       return
     end
     document.name = "Gaceta"
--- app/mailers/document_processing_mailer.rb
+++ app/mailers/document_processing_mailer.rb
@@ -1,9 +1,16 @@
 class DocumentProcessingMailer < ApplicationMailer
 
-  def document_processing_complete(user, document_link)
+  def document_processing_complete(user, document_link, process_status)
     @user = user
     @document_link = document_link
-    mail(from: 'TodoLegal <suscripciones@todolegal.app>', to: @user.email, subject: 'Se ha completado el procesamiento del documento')
+    @process_status = process_status
+    @subject = "Se ha completado el procesamiento del documento"
+    @message = "El documento ha sido procesado con éxito."
+    if @process_status != "success"
+      @message = "Ha ocurrido un error al procesar el documento."
+      @subject = "Error al procesar el documento"
+    end
+    mail(from: 'TodoLegal <suscripciones@todolegal.app>', to: @user.email, subject: @subject)
   end
 
 end

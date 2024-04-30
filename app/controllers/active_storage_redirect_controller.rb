fa2ee6072743e8f92efd15f92da5367a1a5eb28c
carlosvilla00896 <carlosvilla00896@gmail.com>
2024-04-29 23:59:23 -0600

    testing

--- app/controllers/active_storage_redirect_controller.rb
+++ app/controllers/active_storage_redirect_controller.rb
@@ -30,7 +30,7 @@ class ActiveStorageRedirectController < ActiveStorage::Blobs::RedirectController
     end
 
     downloaded_document = Document.find_by_id(params[:document_id])
-    document_type = downloaded_document.document_type.name.downcase.gsub(/\s+/, "")
+    document_type = downloaded_document.document_type.name.downcase.gsub(/\s+/, "-")
     document_type = removeAccents document_type
 
     if user && can_access_document && current_user
@@ -39,7 +39,7 @@ class ActiveStorageRedirectController < ActiveStorage::Blobs::RedirectController
         'document_name' => params[:document_name],
         'document_id' => params[:document_id],
         'location' => "API",
-        'document_url' => `https://valid.todolegal.app/#{document_type}/honduras/#{downloaded_document.url}/#{downloaded_document.id}`
+        'document_url' => `https://test.valid.todolegal.app/#{document_type}/honduras/#{downloaded_document.url}/#{downloaded_document.id}`
       })
       user_trial.save
       super

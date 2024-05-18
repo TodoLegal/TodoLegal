54290fc73b6f68f87579ac33cc3f3255f3095446
Carlos Villanueva <40623613+CarlosVilla00896@users.noreply.github.com>
2024-05-18 01:41:37 -0600

    Fix in document url

--- app/controllers/active_storage_redirect_controller.rb
+++ app/controllers/active_storage_redirect_controller.rb
@@ -29,6 +29,7 @@ class ActiveStorageRedirectController < ActiveStorage::Blobs::RedirectController
       user_trial.downloads += 1
     end
 
+    downloaded_document = Document.find_by_id(params[:document_id])
     document_type = "document"
     if downloaded_document.document_type
       document_type = downloaded_document.document_type?.name.downcase.gsub(/\s+/, "-")
@@ -36,13 +37,15 @@ class ActiveStorageRedirectController < ActiveStorage::Blobs::RedirectController
     end
 
     if user && can_access_document && current_user
-      $tracker.track(user_id, 'Valid download', {
-        'user_type' => current_user_type_api(user),
-        'document_name' => params[:document_name],
-        'document_id' => params[:document_id],
-        'location' => "API",
-        'document_url' => "https://valid.todolegal.app/#{document_type}/honduras/#{downloaded_document.url}/#{downloaded_document.id}"
-      })
+      if downloaded_document
+        $tracker.track(user_id, 'Valid download', {
+          'user_type' => current_user_type_api(user),
+          'document_name' => params[:document_name],
+          'document_id' => params[:document_id],
+          'location' => "API",
+          'document_url' => "https://valid.todolegal.app/#{document_type}/honduras/#{downloaded_document.url}/#{downloaded_document.id}"
+        })
+      end
       user_trial.save
       super
     else

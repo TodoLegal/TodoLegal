db3228fa1d568e4456c50c781e6654182655687c
Carlos Villanueva <carlosvilla00896@gmail.com>
2024-05-31 19:49:14 -0600

    uncomment searchkick line in document model

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,6 @@
 class Document < ApplicationRecord
   include PgSearch
-  # searchkick language: 'spanish'
+  searchkick language: 'spanish'
 
   has_many :issuer_document_tags, :dependent => :destroy
 

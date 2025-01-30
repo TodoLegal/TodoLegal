7e317dea71e12f651a2f1bd33f8c365ba01dd7b0
carlosvilla00896 <carlosvilla00896@gmail.com>
2025-01-30 00:15:33 -0600

    remove name from required fields

--- app/javascript/controllers/document_form_validator_controller.js
+++ app/javascript/controllers/document_form_validator_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="document-form-validator"
 export default class extends Controller {
-  static targets = ["issueId", "name", "shortDescription", "description"]
+  static targets = ["issueId", "shortDescription", "description"]
 
   connect() {
   }

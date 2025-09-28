8fbbb29bf77716d54bc31b779f97beb5fbcb4d17
carlosvilla00896 <carlosvilla00896@gmail.com>
2025-09-28 03:20:12 -0600

    Limit document retrieval to the most recent 50 entries in the documents view

--- test/mailers/previews/notifications_mailer_preview.rb
+++ test/mailers/previews/notifications_mailer_preview.rb
@@ -1,7 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/notifications_mailer
 class NotificationsMailerPreview < ActionMailer::Preview
     def user_preferences_mail
-        NotificationsMailer.user_preferences_mail(User.first, Document.all)
+        NotificationsMailer.user_preferences_mail(User.first, Document.limit(200))
     end
 
     def pro_without_active_notifications

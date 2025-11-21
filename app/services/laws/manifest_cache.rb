0874350b20f9e5a778316a8aa9046f84a7ccd69e
carlosvilla00896 <carlosvilla00896@gmail.com>
2025-11-21 03:23:23 -0600

    Refactor process_single_article_request method and add article retrieval method in ManifestCache

--- app/services/laws/manifest_cache.rb
+++ app/services/laws/manifest_cache.rb
@@ -10,6 +10,7 @@ module Laws
   class ManifestCache
     INLINE_EXPIRES_IN = 12.hours
     FULL_EXPIRES_IN   = 6.hours
+    BASE_VERSION      = 'manifest_v1_chunk100'
 
     # Returns inline subset used at initial render (structure + chunking metadata + version).
     def self.inline_subset(law)
@@ -26,12 +27,18 @@ module Laws
       end
     end
 
+    # Return single article entry by position (0-based)
+    def self.article_by_position(law, position)
+      full_manifest = full(law)
+      full_manifest[:articles].find { |article| article[:position] == position }
+    end
+
     def self.inline_key(law)
-      ["law-manifest-inline", law.id, law.updated_at.to_i]
+      ["law-manifest-inline", BASE_VERSION, law.id, law.updated_at.to_i]
     end
 
     def self.full_key(law)
-      ["law-manifest-full", law.id, law.updated_at.to_i]
+      ["law-manifest-full", BASE_VERSION, law.id, law.updated_at.to_i]
     end
   end
 end

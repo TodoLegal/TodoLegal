a1426919fccf49a60a528529ae7fc94c58ee4408
carlosvilla00896 <carlosvilla00896@gmail.com>
2025-11-24 01:55:05 -0600

    Modify tag model to delete some associated records when destroyed

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,11 +1,11 @@
 class Tag < ApplicationRecord
   belongs_to :tag_type
-  has_many :law_tags
+  has_many :law_tags, dependent: :destroy
   has_many :laws, through: :law_tags
-  has_many :document_tags
+  has_many :document_tags, dependent: :destroy
   has_many :documents, through: :document_tags
-  has_many :issuer_law_tags
-  has_many :issuer_document_tags
+  has_many :issuer_law_tags, dependent: :destroy
+  has_many :issuer_document_tags, dependent: :destroy
   has_many :alternative_tag_names
   has_many :users_preferences_tags
 
